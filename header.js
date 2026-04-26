04607f72f66dad0a12eff029750066eb96042d6f
nav.js <lucian-2000@hotmail.it>
2026-04-26 17:27:46 +0200

    Add premium header to the document on load

---
+++ header.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const headerHTML = `
+    <header class="header-premium">
+        <h1 class="title-green">🙏 Hrana pentru suflet</h1>
+    </header>
+    `;
+    document.body.insertAdjacentHTML("afterbegin", headerHTML);
+});
